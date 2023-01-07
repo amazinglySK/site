981cea6a26d2f60e40d9067d7af9519c12165343
amazinglySK <shashwat07ktm@gmail.com>
2023-01-07 20:39:00 +0400

    Added og_image

--- pages/_app.js
+++ pages/_app.js
@@ -16,6 +16,18 @@ function MyApp({ Component, pageProps }) {
                     href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@300;400;500;600;700&display=swap"
                     rel="stylesheet"
                 />
+                <meta name="description" content="amazinglysk's website 🌐!"/>
+                <meta property="og:url" content="https://amazinglysk.netlify.app"/>
+                <meta property="og:type" content="website"/>
+                <meta property="og:title" content="amazinglySK"/>
+                <meta property="og:description" content="amazinglysk's website 🌐!"/>
+                <meta property="og:image" content="/og_img.png"/>
+                <meta name="twitter:card" content="summary_large_image"/>
+                <meta property="twitter:domain" content="amazisnglysk.netlify.app"/>
+                <meta property="twitter:url" content="https://amazinglysk.netlify.app"/>
+                <meta name="twitter:title" content="amazinglySK"/>
+                <meta name="twitter:description" content="amazinglysk's website 🌐!"/>
+                <meta name="twitter:image" content="/og_img.png"/>
             </Head>
             <Component {...pageProps} />
         </>
