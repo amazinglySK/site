01e8648899d5968765838554c9cc9acffd821f2c
amazinglysk <shashwat07ktm@gmail.com>
2023-04-30 10:12:58 +0400

    Trying out SSR

--- contentful/postHandlers.js
+++ contentful/postHandlers.js
@@ -1,6 +1,4 @@
 import { AppConfig } from "@utils/config";
-import { waitUntilSymbol } from "next/dist/server/web/spec-extension/fetch-event";
-
 const contentful = require("contentful");
 
 const client = contentful.createClient({
@@ -9,21 +7,23 @@ const client = contentful.createClient({
 });
 
 export let LoadAllTags = async () => {
-    let tags = await client.getTags()
+    let tags = await client.getTags();
     const initialVal = {};
-    let reduced = tags.items.reduce((obj, item) => {return {...obj, [item.sys.id] : item.name}}, initialVal)
-    return reduced
-}
+    let reduced = tags.items.reduce((obj, item) => {
+        return { ...obj, [item.sys.id]: item.name };
+    }, initialVal);
+    return reduced;
+};
 
 export let GetPosts = async (iter) => {
     const lim = AppConfig.postsPerLoad;
-    const skipCount = iter*lim
+    const skipCount = iter * lim;
     let res = await client.getEntries({
         content_type: "post",
         select: "sys.id,fields.title,fields.desc,fields.slug,fields.readingTime,metadata.tags",
-        order : "-sys.createdAt",
+        order: "-sys.createdAt",
         limit: lim,
-        skip : skipCount,
+        skip: skipCount,
     });
 
     let resEd = await Promise.all(
@@ -37,7 +37,7 @@ export let GetPosts = async (iter) => {
         })
     );
 
-    let more = resEd.length == lim
+    let more = resEd.length == lim;
 
     return [resEd, more];
 };
@@ -48,8 +48,8 @@ export let GetAPost = async (slug) => {
         select: "fields.title,fields.content,fields.slug,sys.createdAt,sys.updatedAt",
         "fields.slug": slug,
     });
-    res.items[0].sys.createdAt = new Date(res.items[0].sys.createdAt)
-    res.items[0].sys.updatedAt = new Date(res.items[0].sys.updatedAt)
+    res.items[0].sys.createdAt = new Date(res.items[0].sys.createdAt);
+    res.items[0].sys.updatedAt = new Date(res.items[0].sys.updatedAt);
     return res.items[0];
 };
 
@@ -57,6 +57,14 @@ export let GetAllPosts = async () => {
     let res = await client.getEntries({
         content_type: "post",
         select: "sys.id,fields.title,fields.desc,fields.slug,fields.readingTime",
+    });
+    return res.items;
+};
+
+export let GetAllPostSlugs = async () => {
+    let res = await client.getEntries({
+        content_type: "post",
+        select: "fields.slug",
     });
     return res.items;
 };
--- pages/blog/[id].js
+++ pages/blog/[id].js
@@ -1,17 +1,53 @@
 import Post from "@components/Post/Post";
+import { GetAllPostSlugs, GetAPost } from "@contentful/postHandlers";
 import Head from "next/head";
 import Link from "next/link";
+import { remark } from "remark";
+import html from "remark-html";
 
-export default function PostPage() {
+export default function PostPage(props) {
     return (
         <>
             <Head>
-                <title>post | amazinglySK</title>
+                <title>{props.title}</title>
             </Head>
             <div className="container post-page">
-                <Link href = "/blog/">Back to the blog</Link>
-                <Post />
+                <Link href="/blog/">Back to the blog</Link>
+                <Post
+                    content={props.content}
+                    updated={props.updated}
+                    created={props.created}
+                    title={props.title}
+                />
             </div>
         </>
     );
+}
+
+export async function getStaticPaths() {
+    let slugs = await GetAllPostSlugs();
+    const paths = slugs.map(({ fields }) => {
+        return { params: { id: fields.slug } };
+    });
+    return {
+        paths,
+        fallback: false,
+    };
+}
+
+export async function getStaticProps({ params }) {
+    let slug = params.id;
+    let data = await GetAPost(slug);
+    let { title, content } = data.fields;
+    let { createdAt, updatedAt } = data.sys;
+    let parsed = await remark().use(html).process(content);
+    let str_content = parsed.toString();
+    return {
+        props: {
+            title,
+            created: createdAt.toDateString(),
+            updated: updatedAt.toDateString(),
+            content: str_content,
+        },
+    };
 }
